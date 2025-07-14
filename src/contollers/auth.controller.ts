e1c0ca56a3801b3eb3921816e8d56a14beccb070
TamizhanbanT <tamizhanban4563@gmail.com>
2025-07-14 16:09:20 +0530

    2nd commit

--- src/contollers/auth.controller.ts
+++ src/contollers/auth.controller.ts
@@ -1,36 +1,31 @@
 import { Request, Response } from 'express';
 import * as AuthService from '../services/auth.service';
 
-export const showSignup= (req:Request, res:Response) =>{
-    res.render('signup')
+export const showSignup = (req: Request, res: Response) => {
+  const error = req.query.error;
+  res.render('signup', { error });
 };
 
-export const showLogin = (req:Request, res:Response)=>{
-    res.render('login');
+export const showLogin = (req: Request, res: Response) => {
+  const msg = req.query.msg;
+  res.render('login', { msg });
 };
 
-export const handleSignup=async (req:Request, res:Response)=>{
-    try{
-        await AuthService.registerUser(req.body);
-        res.redirect('/login');
-
-    }catch{
-        res.send("email already exist or something went wrong");
-
-    }
+export const handleSignup = async (req: Request, res: Response) => {
+  try {
+    await AuthService.registerUser(req.body);
+    res.redirect('/login?msg=User registered successfully!');
+  } catch {
+    res.redirect('/signup?error=Email already exists or something went wrong');
+  }
 };
 
-export const handleLogin=async (req:Request, res:Response)=>{
-    const user = await AuthService.validateUser(req.body);
-    if(user){
-        req.session.user=user;
-        res.send(`welcome ${user.name}`)
-    }else{
-        res.send("invalid credentials")
-    }
+export const handleLogin = async (req: Request, res: Response) => {
+  const user = await AuthService.validateUser(req.body);
+  if (user) {
+    req.session.user = user;
+    res.send(`Welcome ${user.name}`);
+  } else {
+    res.send("Invalid credentials");
+  }
 };
-
-
-
-
-
