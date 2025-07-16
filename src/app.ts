f61dd68b3831caa978017e2106bcc348eadd27af
TamizhanbanT <tamizhanban4563@gmail.com>
2025-07-16 10:15:35 +0530

    comments

--- src/app.ts
+++ src/app.ts
@@ -7,24 +7,24 @@ import authRoutes from './routes/auth.routes';
 dotenv.config();
 const app = express();
 
-// 1️⃣ Set EJS as view engine
+// Set EJS as view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// 2️⃣ Parse form data (req.body)
+//  Parse form data (req.body)
 app.use(express.urlencoded({ extended: true }));
 
-// 3️⃣ Session middleware ✅ MUST be before routes that use session
+//  Session middleware 
 app.use(session({
   secret: process.env.SESSION_SECRET!, // or 'your_secret_key' (only for testing)
   resave: false,
   saveUninitialized: true,
 }));
 
-// 4️⃣ Your routes
+// Your routes
 app.use(authRoutes);
 
-// 5️⃣ Start server
+// Start server
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
